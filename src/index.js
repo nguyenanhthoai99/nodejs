6faa188b2e8da9418ee186341e59ea85f20c5abe
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2025-12-07 14:32:09 +0700

    i express hahandlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+//HTTP LOGER
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', handlebars({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello World</h1>`)
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
