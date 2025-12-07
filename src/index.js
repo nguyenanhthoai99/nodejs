5b9051e07b42364a784c88ef88b62186c3f00e0a
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2025-12-07 09:59:35 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
