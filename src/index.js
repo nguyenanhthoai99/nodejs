9de6a5b9347881ecdb7c7b918936aa8207bbcc7e
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2025-12-09 10:02:16 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
-route(app);
+route(  app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
