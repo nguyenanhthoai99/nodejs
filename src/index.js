5445d9702946f83beb55cd3e3751456bd20c5635
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2025-12-09 09:38:49 +0700

    update

--- src/index.js
+++ src/index.js
@@ -29,10 +29,12 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-               app.set('views', path.join(__dirname, 'resources/views'));
+     app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
-route(app);
+route(app
+  
+);
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
